e463cb175a04b4d3f874dd01bb31354cf525fca0
Srinjoy Sur <srinjoy_sur@epam.com>
2026-01-09 11:15:22 +0530

    feat: karate basic project structure created

--- karate-practice/target/test-classes/karate-config.js
+++ karate-practice/target/test-classes/karate-config.js
@@ -1,4 +1,4 @@
-function() {    
+function fn() {
   var env = karate.env; // get system property 'karate.env'
   karate.log('karate.env system property was:', env);
   if (!env) {
@@ -6,7 +6,7 @@ function() {
   }
   var config = {
     env: env,
-	myVarName: 'someValue'
+    myVarName: 'someValue'
   }
   if (env == 'dev') {
     // customize
